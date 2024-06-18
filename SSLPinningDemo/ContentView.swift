308e692674ae97acf2a070ec214c1111d61dec51
Krunal Patel <krunal.patel@simformsolutions.com>
2024-06-18 13:01:57 +0530

    Initial commit

---
+++ SSLPinningDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SSLPinningDemo
+//
+//  Created by Krunal Patel on 18/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SSLPinningDemo/SSLPinningDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSLPinningDemoApp.swift
+//  SSLPinningDemo
+//
+//  Created by Krunal Patel on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SSLPinningDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
